9ca0ae382e91c5f0fa4f6d236a8be768d3247364
gotooooo <k.goto.123@gmail.com>
2023-10-28 22:12:31 +0900

    update userdata

--- examples/complete/user_data.sh
+++ examples/complete/user_data.sh
@@ -6,3 +6,15 @@ sudo yum update -y
 # aarch64 / arm64 -> arm64. others -> amd64
 arch=$(arch | grep -q ar && echo "arm64" || echo "amd64")
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_${arch}/amazon-ssm-agent.rpm
+
+# install mysql client 8.0
+sudo dnf localinstall -y https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
+sudo dnf install -y mysql mysql-community-client
+
+# install redis-cli 7.0
+sudo dnf install -y gcc make
+wget http://download.redis.io/releases/redis-7.0.0.tar.gz
+tar xzf redis-7.0.0.tar.gz
+cd redis-7.0.0
+make
+sudo cp src/redis-cli /usr/local/bin/
